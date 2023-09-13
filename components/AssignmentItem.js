dbcad1f409807dbe20c24674ab5e5419ac4000fd
Jimmy Caticat II <20161084@s.ubaguio.edu>
2023-09-13 23:24:51 +0800

    minor: unfinished checkbox

--- components/AssignmentItem.js
+++ components/AssignmentItem.js
@@ -7,32 +7,39 @@ import {
   Image,
   View,
 } from "react-native";
-import React from "react";
-
+import React, { useState } from "react";
+import BouncyCheckbox from "react-native-bouncy-checkbox";
 const deleteImage = require("../assets/delete.png");
 
 const AssignmentItem = ({ task, onDelete }) => {
+  const [checkboxState, setCheckboxState] = useState(false);
+
   return (
     <View style={styles.taskContainer}>
       <TouchableOpacity>
         <Text style={styles.taskText}>{task.text}</Text>
       </TouchableOpacity>
-      <Pressable
-        onPress={() =>
-          Alert.alert("Are you sure?", "", [
-            {
-              text: "Cancel",
-              onPress: () => console.log("canceled"),
-            },
-            {
-              text: "OK",
-              onPress: onDelete,
-            },
-          ])
-        }
-      >
-        <Image source={deleteImage} style={styles.imageContainer}></Image>
-      </Pressable>
+      <View style={styles.buttonGroup}>
+        <BouncyCheckbox
+          onPress={() => setCheckboxState(!checkboxState)}
+          isChecked={checkboxState}
+        />
+        <Pressable
+          onPress={() =>
+            Alert.alert("Are you sure?", "", [
+              {
+                text: "Cancel",
+              },
+              {
+                text: "OK",
+                onPress: onDelete,
+              },
+            ])
+          }
+        >
+          <Image source={deleteImage} style={styles.imageContainer}></Image>
+        </Pressable>
+      </View>
     </View>
   );
 };
@@ -55,9 +62,19 @@ const styles = StyleSheet.create({
     marginLeft: 10,
     color: "#000",
     fontWeight: "bold",
+    paddingVertical: 5,
   },
   imageContainer: {
     width: 36,
     height: 36,
+    marginStart: 10,
+  },
+  buttonGroup: {
+    flexDirection: "row",
+  },
+  checkbox: {
+    padding: 6,
+    paddingEnd: 14,
+    borderRightWidth: 1,
   },
 });
